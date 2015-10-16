5eecde706c6e9a7173a3d44b689f67f2e417ecfd
Rafael Kitover <rkitover@gmail.com>
2015-10-16 16:33:28 -0400

    initial commit

---
+++ lib/dofh.pm
@@ -0,0 +1,46 @@
+use utf8;
+package dofh;
+
+use strict;
+use warnings;
+use 5.008001;
+
+=encoding UTF-8
+
+=head1 NAME
+
+dofh - abstract goes here
+
+=head1 SYNOPSIS
+
+    use dofh;
+
+=head1 DESCRIPTION
+
+=cut
+
+# declarations go here
+
+=head1 FUNCTIONS/METHODS
+
+=head2 some_function
+
+=cut
+
+# some_function goes here
+
+=head1 SEE ALSO
+
+=over 4
+
+=item * L<Some::Module>
+
+=back
+
+=head1 AUTHOR
+
+Rafael Kitover <rkitover@cpan.org>
+
+=cut
+
+__PACKAGE__; # End of dofh
